03972b7f3008ba10451c8aef0274b0c1281f30a9
Luz2011 <021201359b@uandiana.edu.com>
2026-05-11 15:50:32 -0500

    Primer commit: API ClinicaAPI con MySQL

---
+++ Controllers/PacientesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ClinicaAPI.Data;
+using ClinicaAPI.Models;
+using System.Linq;
+
+namespace ClinicaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PacientesController : ControllerBase
+    {
+        private readonly ClinicaContext _context;
+
+        public PacientesController(ClinicaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/pacientes
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var pacientes = _context.Pacientes.ToList();
+            return Ok(pacientes);
+        }
+
+        // POST: api/pacientes
+        [HttpPost]
+        public IActionResult Post([FromBody] Paciente paciente)
+        {
+            if (paciente == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Pacientes.Add(paciente);
+            _context.SaveChanges();
+            return Ok(paciente);
+        }
+    }
+}
