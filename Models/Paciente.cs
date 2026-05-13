15805a50efdc5874f090c991bb92812789b9ad1d
Luz2011 <021201359b@uandiana.edu.com>
2026-05-13 13:30:29 -0500

    Ejercicio 01: Agregar campos Telefono, Direccion, CorreoElectronico, FechaRegistro

--- Models/Paciente.cs
+++ Models/Paciente.cs
@@ -6,5 +6,12 @@
         public string Nombre { get; set; } = string.Empty;
         public int Edad { get; set; }
         public string Diagnostico { get; set; } = string.Empty;
+
+        // NUEVOS CAMPOS
+        public string? Telefono { get; set; }
+        public string? Direccion { get; set; }
+        public string? CorreoElectronico { get; set; }
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
+
     }
 }
